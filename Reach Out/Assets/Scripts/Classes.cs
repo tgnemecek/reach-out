8883c156c38f7eae9288b83edd9d4184fa399b93
Anderson Borba <andersonhborba@hotmail.com>
2019-05-30 20:13:42 -0700

    project created

---
+++ Reach Out/Assets/Scripts/Classes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class message
+{
+    private int value;
+    private int transformationCount;
+
+
+    public void IncreaseCount()
+    {
+        ++transformationCount;
+    }
+
+    public void changeValue(int newValue)
+    {
+        value = newValue;
+    }
+}
---
+++ Reach Out/Assets/Scripts/playerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class playerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int movementSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var xMov = Input.GetAxis("Horizontal") * movementSpeed;
+        var yMov = Input.GetAxis("Vertical") * movementSpeed;
+        rb.velocity = new Vector3(xMov, 0, yMov);
+    }
+}
